ca2b1784b84c0a8a17a9408953f5d181b1467340
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-10-01 23:38:21 -0400

    lint config change

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["prettier/@typescript-eslint", "plugin:prettier/recommended"],
+  rules: {
+    "@typescript-eslint/consistent-type-assertions": "off",
+    "no-native-reassign": "off",
+  },
+};
