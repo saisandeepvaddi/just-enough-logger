0903bc511d54f14025f2ab1def05262dfee37926
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-10-01 23:15:53 -0400

    add tests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { resolve } from 'path';
-import { existsSync, openSync, appendFileSync } from 'fs';
+import { resolve } from "path";
+import { existsSync, openSync, appendFileSync } from "fs";
 
-type Transport = 'file' | 'console';
-type Level = 'info' | 'warn' | 'error';
-interface ILoggerOptions {
+export type Transport = "file" | "console";
+export type Level = "info" | "warn" | "error";
+
+export interface ILoggerOptions {
   transports?: Transport[];
   file?: string;
 }
@@ -17,8 +18,8 @@ interface ILogger {
 
 class Logger implements ILogger {
   options: ILoggerOptions = {
-    transports: ['file', 'console'],
-    file: resolve(__dirname),
+    transports: ["file", "console"],
+    file: resolve(process.cwd(), "log.log"),
   };
 
   constructor(options?: ILoggerOptions) {
@@ -32,22 +33,32 @@ class Logger implements ILogger {
     }
   }
 
+  /**
+   * Returns the log file path for this logger instance.
+   *
+   * @returns {string} Path of log file.
+   * @memberof Logger
+   */
+  public getLogFilePath() {
+    return this.options.file;
+  }
+
   private __getDate() {
     return new Date().toLocaleString();
   }
 
   private __shouldWriteToFile(): boolean {
-    return this.options.transports!.indexOf('file') > -1;
+    return this.options.transports!.indexOf("file") > -1;
   }
 
   private __shouldWriteToConsole(): boolean {
-    return this.options.transports!.indexOf('console') > -1;
+    return this.options.transports!.indexOf("console") > -1;
   }
 
   private __createLogFile() {
     const { file } = this.options;
     if (file && !existsSync(file)) {
-      openSync(file, 'w');
+      openSync(file, "w");
     }
   }
 
@@ -62,14 +73,14 @@ class Logger implements ILogger {
     console[level](message);
   }
 
-  private __formatMessage(message: string, level: Level) {
+  public formatter(message: string, level: Level) {
     return `${this.__getDate()} : [${level.toUpperCase()}] : ${message}`;
   }
 
   public info(message: string) {
-    let formattedMessage = this.__formatMessage(message, 'info');
+    let formattedMessage = this.formatter(message, "info");
     if (this.__shouldWriteToConsole()) {
-      this.__writeToConsole(formattedMessage, 'info');
+      this.__writeToConsole(formattedMessage, "info");
     }
     if (this.__shouldWriteToFile()) {
       this.__writeToFile(formattedMessage);
@@ -77,9 +88,9 @@ class Logger implements ILogger {
   }
 
   public warn(message: string) {
-    let formattedMessage = this.__formatMessage(message, 'warn');
+    let formattedMessage = this.formatter(message, "warn");
     if (this.__shouldWriteToConsole()) {
-      this.__writeToConsole(formattedMessage, 'warn');
+      this.__writeToConsole(formattedMessage, "warn");
     }
     if (this.__shouldWriteToFile()) {
       this.__writeToFile(formattedMessage);
@@ -87,9 +98,9 @@ class Logger implements ILogger {
   }
 
   public error(message: string) {
-    let formattedMessage = this.__formatMessage(message, 'error');
+    let formattedMessage = this.formatter(message, "error");
     if (this.__shouldWriteToConsole()) {
-      this.__writeToConsole(formattedMessage, 'error');
+      this.__writeToConsole(formattedMessage, "error");
     }
     if (this.__shouldWriteToFile()) {
       this.__writeToFile(formattedMessage);
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,186 @@
-import Logger from '../src';
+import Logger, { ILoggerOptions } from "../src";
+import fs from "fs";
+import { resolve } from "path";
 
-jest.genMockFromModule('fs');
-jest.genMockFromModule('console');
+jest.mock("fs");
+let originalConsole = global.console;
 
-test("creates a log file if doesn't exist", () => {
-  let options = {
-    transports: ['file', 'console'],
-  };
+global.console = {
+  ...global.console,
+  info: jest.fn(str => str),
+  warn: jest.fn(str => str),
+  error: jest.fn(str => str),
+};
+
+describe("All Tests", () => {
+  test("creates a log file if doesn't exist", () => {
+    let options: ILoggerOptions = {
+      transports: ["file", "console"],
+    };
+    new Logger(options);
+    expect(fs.existsSync).toHaveBeenCalled();
+    expect(fs.openSync).toHaveBeenCalled();
+  });
+
+  test("only uses specified transports", () => {
+    let fileLogger = new Logger({
+      transports: ["file"],
+    });
+
+    fileLogger.info("Test Message");
+    expect(fs.existsSync).toHaveBeenCalled();
+    expect(fs.openSync).toHaveBeenCalled();
+    expect(fs.appendFileSync).toHaveBeenCalled();
+
+    expect(console.info).not.toHaveBeenCalled();
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(console.error).not.toHaveBeenCalled();
+
+    (fs.existsSync as jest.Mock).mockReset();
+    (fs.openSync as jest.Mock).mockReset();
+    (fs.appendFileSync as jest.Mock).mockReset();
+
+    let consoleLogger = new Logger({
+      transports: ["console"],
+    });
+
+    consoleLogger.info("Test Message");
+    consoleLogger.warn("Test Message");
+    consoleLogger.error("Test Message");
+
+    expect(fs.existsSync).not.toHaveBeenCalled();
+    expect(fs.openSync).not.toHaveBeenCalled();
+    expect(fs.appendFileSync).not.toHaveBeenCalled();
+
+    expect(console.info).toHaveBeenCalled();
+    expect(console.warn).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalled();
+  });
+
+  test("creates a log file at given path", () => {
+    let logFilePath = resolve(__dirname, "test.log");
+    new Logger({
+      file: logFilePath,
+    });
+
+    expect(fs.existsSync).toHaveBeenCalledWith(logFilePath);
+    expect(fs.openSync).toHaveBeenCalledWith(logFilePath, "w");
+  });
+
+  test("does not create same log file more than once", () => {
+    let logFilePath = resolve(__dirname, "test.log");
+
+    fs.existsSync = jest
+      .fn()
+      .mockReturnValueOnce(false)
+      .mockReturnValueOnce(true);
+
+    new Logger({
+      file: logFilePath,
+    });
+
+    new Logger({
+      file: logFilePath,
+    });
+
+    expect(fs.existsSync).toHaveBeenCalledTimes(2);
+    expect(fs.openSync).toHaveBeenCalledTimes(1);
+  });
+
+  test("returns correct log file path", () => {
+    let defaultFilePath = resolve(process.cwd(), "log.log");
+    let customFilePath = resolve(__dirname, "test.log");
+
+    let logger = new Logger();
+    expect(logger.getLogFilePath()).toBe(defaultFilePath);
+
+    let customFileLogger = new Logger({
+      file: customFilePath,
+    });
+
+    expect(customFileLogger.getLogFilePath()).toBe(customFilePath);
+  });
+
+  test("prints to console with correct level", () => {
+    let logger = new Logger({
+      transports: ["console"],
+    });
+
+    let message = "Test message";
+    logger.info(message);
+    let infoMessage = logger.formatter(message, "info");
+    logger.warn(message);
+    let warnMessage = logger.formatter(message, "warn");
+    logger.error(message);
+    let errorMessage = logger.formatter(message, "error");
+
+    expect(console.info).toHaveBeenCalledWith(infoMessage);
+    expect(console.warn).toHaveBeenCalledWith(warnMessage);
+    expect(console.error).toHaveBeenCalledWith(errorMessage);
+  });
+
+  test("logs to file with correct level", () => {
+    let logFilePath = resolve(__dirname, "test.log");
+
+    let logger = new Logger({
+      transports: ["file"],
+      file: logFilePath,
+    });
+
+    let message = "Test message";
+    logger.info(message);
+    let infoMessage = logger.formatter(message, "info");
+    logger.warn(message);
+    let warnMessage = logger.formatter(message, "warn");
+    logger.error(message);
+    let errorMessage = logger.formatter(message, "error");
+
+    expect(fs.appendFileSync).toHaveBeenCalledWith(logFilePath, infoMessage);
+    expect(fs.appendFileSync).toHaveBeenCalledWith(logFilePath, warnMessage);
+    expect(fs.appendFileSync).toHaveBeenCalledWith(logFilePath, errorMessage);
+  });
+
+  test("will not write to file if file is empty even with transport", () => {
+    new Logger({
+      transports: ["file"],
+      file: "",
+    });
+
+    expect(fs.existsSync).not.toHaveBeenCalled();
+    expect(fs.appendFileSync).not.toHaveBeenCalled();
+  });
+
+  test("prints on both transports", () => {
+    let logFilePath = resolve(__dirname, "test.log");
+
+    let logger = new Logger({
+      transports: ["console", "file"],
+      file: logFilePath,
+    });
+
+    let message = "Test message";
+    logger.info(message);
+    let infoMessage = logger.formatter(message, "info");
+    logger.warn(message);
+    let warnMessage = logger.formatter(message, "warn");
+    logger.error(message);
+    let errorMessage = logger.formatter(message, "error");
+
+    expect(console.info).toHaveBeenCalledWith(infoMessage);
+    expect(console.warn).toHaveBeenCalledWith(warnMessage);
+    expect(console.error).toHaveBeenCalledWith(errorMessage);
+
+    expect(fs.appendFileSync).toHaveBeenCalledWith(logFilePath, infoMessage);
+    expect(fs.appendFileSync).toHaveBeenCalledWith(logFilePath, warnMessage);
+    expect(fs.appendFileSync).toHaveBeenCalledWith(logFilePath, errorMessage);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+    console = originalConsole;
+  });
 });
